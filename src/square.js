eab6db82c7d03a069a1f695a4bf4afeca0029fe5
Michał Stróż <michalstroz@gmail.com>
2019-01-28 23:09:31 +0100

    add new className depends of boolean value of win property in Square component

--- src/square.js
+++ src/square.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>{props.value}</button>
-  );
+  let setColor = props.win
+    ? 'winner-squares'
+    : ''
+  return (<button className={`square ${setColor}`} onClick={props.onClick}>{props.value}</button>);
 }
 
 export default Square;
